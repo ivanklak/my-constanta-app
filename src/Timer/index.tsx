4ae6d4ef83743a7acea02675a673442e07532acd
ivanklak <ivanklak17@gmail.com>
2022-04-09 00:17:14 +0400

    refactor: add copy time

--- src/Timer/index.tsx
+++ src/Timer/index.tsx
@@ -6,12 +6,18 @@ import {error} from "../App/helper/setter";
 import styles from './styles.module.css';
 
 const Time: FC = () => {
-
     const [time, setTime] = useState(0);
+    const [notice, setNotice] = useState('');
 
     const clickHandler = () => {
         timer.resetTimer();
     };
+
+    const copyHandler = () => {
+        navigator.clipboard.writeText(String(time));
+        setNotice('Time copied to clipboard');
+        setTimeout(() => setNotice(''), 1000);
+    }
 
     const storageHandler = function () {
         setTime(Number(localStorage.getItem('passedTime')));
@@ -26,10 +32,12 @@ const Time: FC = () => {
     return (
         <div className={styles.timerContainer}>
             <div className={styles.timer}>
-                <span>{time}</span>
-                <div className={styles.resetButton}>
+                <span id="time">{time}</span>
+                <div className={styles.timerButtonsField}>
                     <button onClick={clickHandler}>Reset Time</button>
+                    <button onClick={copyHandler}>Copy time</button>
                 </div>
+                {notice && <div className={styles.notice}>{notice}</div>}
                 <div className={styles.error}>{error.setError}</div>
             </div>
         </div>
