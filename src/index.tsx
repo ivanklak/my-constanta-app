fc1a2a99d87de8c416905ae29def0db9e377e941
ivanklak <ivanklak17@gmail.com>
2022-04-09 13:47:18 +0400

    refactor: add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 import App from './App'
@@ -9,10 +9,10 @@ import store from "./App/redux-store";
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter basename='/my-constanta-app'>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 )
